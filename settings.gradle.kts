e7428a75cd256ab8f8be5820e8ae3e2fa11f1cfc
Sadikur Rahman <sadikurbd02@gmail.com>
2026-01-16 15:51:56 +0600

    Fix settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,13 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+    
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
