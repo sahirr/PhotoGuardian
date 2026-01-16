50adc6499b8c2fcfe22a3ec22108b6bb80715340
Sadikur Rahman <sadikurbd02@gmail.com>
2026-01-16 19:08:26 +0600

    fix theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     
+    implementation("com.google.android.material:material:1.11.0")
+    
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.6")
 
