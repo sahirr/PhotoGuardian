ce6faf391368fb92d0a884bbd9f8bb1fbe7740f9
Sadikur Rahman <sadikurbd02@gmail.com>
2026-01-16 16:04:39 +0600

    Fix VerCat

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,17 +6,17 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
     
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+    
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
     }
 }
 
